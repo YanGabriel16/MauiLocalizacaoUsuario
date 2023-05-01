6ddfdaba0d8256e9d5db1d823709416fcdf8eec1
yan <yan80ky16@gmail.com>
2023-05-01 10:40:05 -0300

    TCC :: Coordenadas atuais

---
+++ LocationService.cs
@@ -0,0 +1,56 @@
+﻿using ficym.Services.location;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Devices.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ficym
+{
+    public class LocationService : ILocationService
+    {
+        public async Task<GeoCordinates> GetCurrenteLocation(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Location service passou...");
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(1));
+                var location = await Geolocation.GetLocationAsync(request, cancellationToken);
+
+                if (location != null)
+                {
+                    return new GeoCordinates()
+                    {
+                        Latitude = location.Latitude,
+                        Longitude = location.Longitude,
+                        Altitude = location.Altitude,
+                    };
+                }
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FeatureNotEnabledException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (PermissionException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return new GeoCordinates()
+            {
+                Latitude = 9999999.00,
+                Longitude = 9999999.00,
+                Altitude = 9999999.00,
+            };
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ficym;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
